4ab11b01e7c732065a1df4be0bf043cc25b1e7d9
Ahmed Abdoulaye <ahmed.abdoulaye@gmail.com>
2016-07-18 13:58:29 -0600

    Linked reviews with restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716164140) do
+ActiveRecord::Schema.define(version: 20160718182610) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20160716164140) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
